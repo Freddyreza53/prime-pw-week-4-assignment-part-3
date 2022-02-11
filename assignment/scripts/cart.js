2a97ca504e27c6c5046d8bc81ae7593d735509e3
Freddy Reza <rezafreddy@gmail.com>
2022-02-10 20:52:46 -0600

    I shortened the commented out lines from 47 and 49 to make it look better.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -44,8 +44,10 @@ console.log('***** Cart Functions *****');
 //
 // 4. Create a function called `removeItem`. It should:
 //   - Take an input parameter for a string `item`
-//   - Use [Array.indexOf](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf) to find the index of the first matching item in the basket.
-//   - Use [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to remove the first matching item from the basket.
+//   - Use [Array.indexOf](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
+//       to find the index of the first matching item in the basket.
+//   - Use [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice)
+//       to remove the first matching item from the basket.
 //   - Return the item removed or `null` if the item was not found
 let basket = [];
 const maxItems = 5;
